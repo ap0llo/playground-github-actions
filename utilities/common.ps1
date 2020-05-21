0fb044f2c5232c0b0cc681060547e6b0002e3270
Andreas Gruenwald <andreas@grynwald.net>
2020-05-22 00:16:50 +0200

    Reimplement tool update and PR creation in powershell

---
+++ utilities/common.ps1
@@ -0,0 +1,92 @@
+function Start-Command {
+
+    param(
+        [Parameter(Mandatory = $true)][string]$Command,
+        [Parameter(Mandatory= $false)][switch]$SkipExitCodeCheck
+    )
+
+    Invoke-Expression $Command | Write-Host
+    if(-not $SkipExitCodeCheck) {
+        if ($LASTEXITCODE -ne 0) {
+            throw "Command '$command' completed with exit code $LASTEXITCODE"
+        }
+    }
+}
+
+function Get-ToolName {
+
+    param(
+        [Parameter(Mandatory = $true)][string]$ManifestPath
+    )
+
+    $json = Get-Content -Raw -Path $ManifestPath | ConvertFrom-Json
+    $toolNames = $json.tools | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+    return $toolNames
+}
+
+
+function Get-ToolVersion {
+
+    param(
+        [Parameter(Mandatory = $true)][string]$ManifestPath,
+        [Parameter(Mandatory = $true)][string]$ToolName
+    )
+
+    $json = Get-Content -Raw -Path $ManifestPath | ConvertFrom-Json
+    $toolVersion = $json.tools.$ToolName.version
+    if(-not $toolVersion) {
+        throw "Failed to determine version of tool $ToolName"
+    }
+    return $toolVersion
+}
+
+
+function Write-Log ($message) {
+    Write-Host "[LOG] $message" -ForegroundColor Green     
+}
+
+
+function Reset-WorkingCopy {
+
+    Write-Log "Resetting working copy"
+    Start-Command "git reset --hard" 
+    Start-Command "git diff --quiet" # ensure working copy is clean
+
+}
+
+
+function Update-Tool {
+
+    param(
+        [Parameter(Mandatory = $true)][string]$ManifestPath,
+        [Parameter(Mandatory = $true)][string]$ToolName
+    )
+
+    $manifestDir = Split-Path -Path $ManifestPath -Parent
+
+    Push-Location $manifestDir
+    try {
+        Write-Log "Updating $ToolName" 
+        Start-Command "dotnet tool update `"$toolName`" "
+    }
+    finally {
+        Pop-Location
+    }
+    
+    Start-Command "git diff --quiet" -SkipExitCodeCheck
+    if($LASTEXITCODE -ne 0) {
+        $version = Get-ToolVersion -MainfestPath $ManifestPath -ToolName $toolName
+        Write-Log "Tool '$toolName' was updated to version $version"
+
+        $branchName = "toolupdates/$toolName-$version"
+        Write-Log "Creating branch '$branchName'"
+        Start-Command "git checkout -b `"$branchName`""
+
+        Start-Command "git commit -am `"build(deps): Update $toolName to version $version`"" 
+        Start-Command "git checkout -"
+
+        return $branchName
+    } else {
+        return $null
+    }
+}
---
+++ utilities/update-tools.ps1
@@ -0,0 +1,52 @@
+
+
+# Import Helper functions
+. (Join-Path $PSScriptRoot "common.ps1") 
+
+# Install dependencies
+Install-Module -Name PowerShellForGitHub -Force
+
+# TODO: configure credentials
+
+# Set up default repository and owner
+if($env:GITHUB_REPOSITORY) {
+    Write-Log "Setting default owner and repository to '$env:GITHUB_REPOSITORY' "
+    $ownerAndRepo = $env:GITHUB_REPOSITORY.Split('/')
+    Set-GitHubConfiguration -DefaultOwnerName $ownerAndRepo[0] -DefaultRepositoryName $ownerAndRepo[1]
+} else {
+    throw "Failed to determine default owner and repository. Expected environment variable 'GITHUB_REPOSITORY' to be set"
+}
+
+if($env:GITHUB_TOKEN) {
+    $user = "GitHub Actions"
+    $accessToken = ConvertTo-SecureString -String $env:GITHUB_TOKEN -AsPlainText -Force
+    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user,$accessToken
+    Set-GitHubAuthentication -Credential $credential
+} else {
+    throw "Failed to set authentication token Expected environment variable 'GITHUB_TOKEN' to be set"
+}
+
+
+# Variables
+$repoRoot = (Join-Path $PSScriptRoot ".." | Resolve-Path).Path
+$toolManifestPath = (Join-Path $repoRoot "dotnet-tools.json")
+$targetBranch = "master"
+
+# Main script
+$toolNames = Get-ToolName -ManifestPath $toolManifestPath
+
+
+foreach($toolName in $toolNames) {
+    
+    Reset-WorkingCopy
+    $newBranchName = Update-Tool -ManifestPath $toolManifestPath -ToolName $toolName
+
+    if($newBranchName) {
+        # Push the new branch
+        Start-Command "git push origin $newBranchName:$newBranchName"
+
+        # Create a Pull Request for the branch
+        $pr = New-GitHubPullRequest -Title "Update tool $toolName" -Head $newBranchName -Base $targetBranch
+    }
+}
+
